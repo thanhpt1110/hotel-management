6043ada3aa949f993c9e933a44888391464d3090
thtuanlegithub <thanhtuanse16@gmail.com>
2022-12-02 23:16:49 +0700

    add custom datepicker

---
+++ HotelManagement/GUI/FormDatPhong.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace HotelManagement.GUI
+{
+    partial class FormDatPhong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ctTextBox5 = new HotelManagement.CTControls.CTTextBox();
+            this.ctTextBox4 = new HotelManagement.CTControls.CTTextBox();
+            this.ctTextBox3 = new HotelManagement.CTControls.CTTextBox();
+            this.ctTextBox2 = new HotelManagement.CTControls.CTTextBox();
+            this.ctTextBox1 = new HotelManagement.CTControls.CTTextBox();
+            this.LabelThongTinPhong = new System.Windows.Forms.Label();
+            this.LabelThongTinKH = new System.Windows.Forms.Label();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.LabelDatPhong = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.ctTextBox5);
+            this.panel1.Controls.Add(this.ctTextBox4);
+            this.panel1.Controls.Add(this.ctTextBox3);
+            this.panel1.Controls.Add(this.ctTextBox2);
+            this.panel1.Controls.Add(this.ctTextBox1);
+            this.panel1.Controls.Add(this.LabelThongTinPhong);
+            this.panel1.Controls.Add(this.LabelThongTinKH);
+            this.panel1.Controls.Add(this.splitter1);
+            this.panel1.Location = new System.Drawing.Point(36, 52);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(887, 447);
+            this.panel1.TabIndex = 0;
+            // 
+            // ctTextBox5
+            // 
+            this.ctTextBox5.BackColor = System.Drawing.SystemColors.Control;
+            this.ctTextBox5.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ctTextBox5.BorderFocusColor = System.Drawing.Color.Black;
+            this.ctTextBox5.BorderRadius = 0;
+            this.ctTextBox5.BorderSize = 1;
+            this.ctTextBox5.IsFocused = false;
+            this.ctTextBox5.Location = new System.Drawing.Point(85, 333);
+            this.ctTextBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.ctTextBox5.Multiline = false;
+            this.ctTextBox5.Name = "ctTextBox5";
+            this.ctTextBox5.Padding = new System.Windows.Forms.Padding(7);
+            this.ctTextBox5.PasswordChar = false;
+            this.ctTextBox5.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.ctTextBox5.PlaceholderText = "";
+            this.ctTextBox5.Size = new System.Drawing.Size(205, 28);
+            this.ctTextBox5.TabIndex = 2;
+            this.ctTextBox5.Texts = "";
+            this.ctTextBox5.UnderlineedStyle = true;
+            // 
+            // ctTextBox4
+            // 
+            this.ctTextBox4.BackColor = System.Drawing.SystemColors.Control;
+            this.ctTextBox4.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ctTextBox4.BorderFocusColor = System.Drawing.Color.Black;
+            this.ctTextBox4.BorderRadius = 0;
+            this.ctTextBox4.BorderSize = 1;
+            this.ctTextBox4.IsFocused = false;
+            this.ctTextBox4.Location = new System.Drawing.Point(85, 260);
+            this.ctTextBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.ctTextBox4.Multiline = false;
+            this.ctTextBox4.Name = "ctTextBox4";
+            this.ctTextBox4.Padding = new System.Windows.Forms.Padding(7);
+            this.ctTextBox4.PasswordChar = false;
+            this.ctTextBox4.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.ctTextBox4.PlaceholderText = "";
+            this.ctTextBox4.Size = new System.Drawing.Size(205, 28);
+            this.ctTextBox4.TabIndex = 2;
+            this.ctTextBox4.Texts = "";
+            this.ctTextBox4.UnderlineedStyle = true;
+            // 
+            // ctTextBox3
+            // 
+            this.ctTextBox3.BackColor = System.Drawing.SystemColors.Control;
+            this.ctTextBox3.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ctTextBox3.BorderFocusColor = System.Drawing.Color.Black;
+            this.ctTextBox3.BorderRadius = 0;
+            this.ctTextBox3.BorderSize = 1;
+            this.ctTextBox3.IsFocused = false;
+            this.ctTextBox3.Location = new System.Drawing.Point(85, 192);
+            this.ctTextBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.ctTextBox3.Multiline = false;
+            this.ctTextBox3.Name = "ctTextBox3";
+            this.ctTextBox3.Padding = new System.Windows.Forms.Padding(7);
+            this.ctTextBox3.PasswordChar = false;
+            this.ctTextBox3.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.ctTextBox3.PlaceholderText = "";
+            this.ctTextBox3.Size = new System.Drawing.Size(205, 28);
+            this.ctTextBox3.TabIndex = 2;
+            this.ctTextBox3.Texts = "";
+            this.ctTextBox3.UnderlineedStyle = true;
+            // 
+            // ctTextBox2
+            // 
+            this.ctTextBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.ctTextBox2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ctTextBox2.BorderFocusColor = System.Drawing.Color.Black;
+            this.ctTextBox2.BorderRadius = 0;
+            this.ctTextBox2.BorderSize = 1;
+            this.ctTextBox2.IsFocused = false;
+            this.ctTextBox2.Location = new System.Drawing.Point(85, 127);
+            this.ctTextBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.ctTextBox2.Multiline = false;
+            this.ctTextBox2.Name = "ctTextBox2";
+            this.ctTextBox2.Padding = new System.Windows.Forms.Padding(7);
+            this.ctTextBox2.PasswordChar = false;
+            this.ctTextBox2.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.ctTextBox2.PlaceholderText = "";
+            this.ctTextBox2.Size = new System.Drawing.Size(205, 28);
+            this.ctTextBox2.TabIndex = 2;
+            this.ctTextBox2.Texts = "";
+            this.ctTextBox2.UnderlineedStyle = true;
+            // 
+            // ctTextBox1
+            // 
+            this.ctTextBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.ctTextBox1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ctTextBox1.BorderFocusColor = System.Drawing.Color.Black;
+            this.ctTextBox1.BorderRadius = 0;
+            this.ctTextBox1.BorderSize = 1;
+            this.ctTextBox1.IsFocused = false;
+            this.ctTextBox1.Location = new System.Drawing.Point(85, 67);
+            this.ctTextBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.ctTextBox1.Multiline = false;
+            this.ctTextBox1.Name = "ctTextBox1";
+            this.ctTextBox1.Padding = new System.Windows.Forms.Padding(7);
+            this.ctTextBox1.PasswordChar = false;
+            this.ctTextBox1.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.ctTextBox1.PlaceholderText = "";
+            this.ctTextBox1.Size = new System.Drawing.Size(205, 28);
+            this.ctTextBox1.TabIndex = 2;
+            this.ctTextBox1.Texts = "";
+            this.ctTextBox1.UnderlineedStyle = true;
+            // 
+            // LabelThongTinPhong
+            // 
+            this.LabelThongTinPhong.AutoSize = true;
+            this.LabelThongTinPhong.BackColor = System.Drawing.Color.Transparent;
+            this.LabelThongTinPhong.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelThongTinPhong.Location = new System.Drawing.Point(469, 15);
+            this.LabelThongTinPhong.Name = "LabelThongTinPhong";
+            this.LabelThongTinPhong.Size = new System.Drawing.Size(206, 32);
+            this.LabelThongTinPhong.TabIndex = 1;
+            this.LabelThongTinPhong.Text = "Thông tin phòng";
+            // 
+            // LabelThongTinKH
+            // 
+            this.LabelThongTinKH.AutoSize = true;
+            this.LabelThongTinKH.BackColor = System.Drawing.Color.Transparent;
+            this.LabelThongTinKH.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelThongTinKH.Location = new System.Drawing.Point(18, 15);
+            this.LabelThongTinKH.Name = "LabelThongTinKH";
+            this.LabelThongTinKH.Size = new System.Drawing.Size(262, 32);
+            this.LabelThongTinKH.TabIndex = 1;
+            this.LabelThongTinKH.Text = "Thông tin khách hàng";
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(305, 447);
+            this.splitter1.TabIndex = 0;
+            this.splitter1.TabStop = false;
+            // 
+            // LabelDatPhong
+            // 
+            this.LabelDatPhong.AutoSize = true;
+            this.LabelDatPhong.BackColor = System.Drawing.Color.White;
+            this.LabelDatPhong.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelDatPhong.Location = new System.Drawing.Point(400, 7);
+            this.LabelDatPhong.Name = "LabelDatPhong";
+            this.LabelDatPhong.Size = new System.Drawing.Size(153, 37);
+            this.LabelDatPhong.TabIndex = 1;
+            this.LabelDatPhong.Text = "Đặt Phòng";
+            // 
+            // FormDatPhong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(959, 558);
+            this.Controls.Add(this.LabelDatPhong);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormDatPhong";
+            this.Text = "FormDatPhong";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private CTControls.CTTextBox ctTextBox5;
+        private CTControls.CTTextBox ctTextBox4;
+        private CTControls.CTTextBox ctTextBox3;
+        private CTControls.CTTextBox ctTextBox2;
+        private CTControls.CTTextBox ctTextBox1;
+        private System.Windows.Forms.Label LabelThongTinPhong;
+        private System.Windows.Forms.Label LabelThongTinKH;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.Label LabelDatPhong;
+    }
+}
