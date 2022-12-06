be62b405f70ec0251896c9d5a8959bc2c18731c1
thtuanlegithub <thanhtuanse16@gmail.com>
2022-12-06 19:30:53 +0700

    add MessageBoxThongBao

--- HotelManagement/Properties/Resources.Designer.cs
+++ HotelManagement/Properties/Resources.Designer.cs
@@ -363,6 +363,16 @@ namespace HotelManagement.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap message {
+            get {
+                object obj = ResourceManager.GetObject("message", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap money {
             get {
                 object obj = ResourceManager.GetObject("money", resourceCulture);
