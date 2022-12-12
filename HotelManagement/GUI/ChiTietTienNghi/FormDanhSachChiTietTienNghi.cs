be927f58d887ed115b1fb27fcdcd43bced946d9c
Tuan-Thanh Phan <thanhphantuan1110@gmail.com>
2022-12-12 14:07:40 +0700

    Update FormDanhSachCTTN

--- HotelManagement/GUI/ChiTietTienNghi/FormDanhSachChiTietTienNghi.cs
+++ HotelManagement/GUI/ChiTietTienNghi/FormDanhSachChiTietTienNghi.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,15 +19,239 @@ namespace HotelManagement.GUI
         private Image edit = Properties.Resources.edit;
         private Image delete = Properties.Resources.delete;
         private FormMain formMain;
+
+        //Constructor
         public FormDanhSachChiTietTienNghi()
         {
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Padding = new Padding(borderSize);
             InitializeComponent();
         }
 
         public FormDanhSachChiTietTienNghi(FormMain formMain)
         {
-            InitializeComponent();
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Padding = new Padding(borderSize);
             this.formMain = formMain;
+            InitializeComponent();
+        }
+
+        //Fields
+        private int borderRadius = 20;
+        private int borderSize = 2;
+        private Color borderColor = Color.White;
+
+        //Control Box
+
+        //Form Move
+
+        //Drag Form
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.Style |= 0x20000; // <--- Minimize borderless form from taskbar
+                return cp;
+            }
+        }
+
+        //Private Methods
+        //Private Methods
+        private GraphicsPath GetRoundedPath(Rectangle rect, float radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            float curveSize = radius * 2F;
+            path.StartFigure();
+            path.AddArc(rect.X, rect.Y, curveSize, curveSize, 180, 90);
+            path.AddArc(rect.Right - curveSize, rect.Y, curveSize, curveSize, 270, 90);
+            path.AddArc(rect.Right - curveSize, rect.Bottom - curveSize, curveSize, curveSize, 0, 90);
+            path.AddArc(rect.X, rect.Bottom - curveSize, curveSize, curveSize, 90, 90);
+            path.CloseFigure();
+            return path;
+        }
+        private void ControlRegionAndBorder(Control control, float radius, Graphics graph, Color borderColor)
+        {
+            using (GraphicsPath roundPath = GetRoundedPath(control.ClientRectangle, radius))
+            using (Pen penBorder = new Pen(borderColor, 1))
+            {
+                graph.SmoothingMode = SmoothingMode.AntiAlias;
+                control.Region = new Region(roundPath);
+                graph.DrawPath(penBorder, roundPath);
+            }
+        }
+        private void FormRegionAndBorder(Form form, float radius, Graphics graph, Color borderColor, float borderSize)
+        {
+            if (this.WindowState != FormWindowState.Minimized)
+            {
+                using (GraphicsPath roundPath = GetRoundedPath(form.ClientRectangle, radius))
+                using (Pen penBorder = new Pen(borderColor, borderSize))
+                using (Matrix transform = new Matrix())
+                {
+                    graph.SmoothingMode = SmoothingMode.AntiAlias;
+                    form.Region = new Region(roundPath);
+                    if (borderSize >= 1)
+                    {
+                        Rectangle rect = form.ClientRectangle;
+                        float scaleX = 1.0F - ((borderSize + 1) / rect.Width);
+                        float scaleY = 1.0F - ((borderSize + 1) / rect.Height);
+                        transform.Scale(scaleX, scaleY);
+                        transform.Translate(borderSize / 1.6F, borderSize / 1.6F);
+                        graph.Transform = transform;
+                        graph.DrawPath(penBorder, roundPath);
+                    }
+                }
+            }
+        }
+        private void DrawPath(Rectangle rect, Graphics graph, Color color)
+        {
+            using (GraphicsPath roundPath = GetRoundedPath(rect, borderRadius))
+            using (Pen penBorder = new Pen(color, 3))
+            {
+                graph.DrawPath(penBorder, roundPath);
+            }
+        }
+        private struct FormBoundsColors
+        {
+            public Color TopLeftColor;
+            public Color TopRightColor;
+            public Color BottomLeftColor;
+            public Color BottomRightColor;
+        }
+        private FormBoundsColors GetFormBoundsColors()
+        {
+            var fbColor = new FormBoundsColors();
+            using (var bmp = new Bitmap(1, 1))
+            using (Graphics graph = Graphics.FromImage(bmp))
+            {
+                Rectangle rectBmp = new Rectangle(0, 0, 1, 1);
+                //Top Left
+                rectBmp.X = this.Bounds.X - 1;
+                rectBmp.Y = this.Bounds.Y;
+                graph.CopyFromScreen(rectBmp.Location, Point.Empty, rectBmp.Size);
+                fbColor.TopLeftColor = bmp.GetPixel(0, 0);
+                //Top Right
+                rectBmp.X = this.Bounds.Right;
+                rectBmp.Y = this.Bounds.Y;
+                graph.CopyFromScreen(rectBmp.Location, Point.Empty, rectBmp.Size);
+                fbColor.TopRightColor = bmp.GetPixel(0, 0);
+                //Bottom Left
+                rectBmp.X = this.Bounds.X;
+                rectBmp.Y = this.Bounds.Bottom;
+                graph.CopyFromScreen(rectBmp.Location, Point.Empty, rectBmp.Size);
+                fbColor.BottomLeftColor = bmp.GetPixel(0, 0);
+                //Bottom Right
+                rectBmp.X = this.Bounds.Right;
+                rectBmp.Y = this.Bounds.Bottom;
+                graph.CopyFromScreen(rectBmp.Location, Point.Empty, rectBmp.Size);
+                fbColor.BottomRightColor = bmp.GetPixel(0, 0);
+            }
+            return fbColor;
+        }
+        private FormBoundsColors GetSameDark()
+        {
+            FormBoundsColors colors = new FormBoundsColors();
+            colors.TopLeftColor = Color.FromArgb(67, 73, 73);
+            colors.TopRightColor = Color.FromArgb(67, 73, 73);
+            colors.BottomLeftColor = Color.FromArgb(67, 73, 73);
+            colors.BottomRightColor = Color.FromArgb(67, 73, 73);
+            return colors;
+        }
+        //Event Methods
+        private void FormDanhSachChiTietTienNghi_Paint(object sender, PaintEventArgs e)
+        {
+            //-> SMOOTH OUTER BORDER
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            Rectangle rectForm = this.ClientRectangle;
+            int mWidht = rectForm.Width / 2;
+            int mHeight = rectForm.Height / 2;
+            var fbColors = GetSameDark();
+            //Top Left
+            DrawPath(rectForm, e.Graphics, fbColors.TopLeftColor);
+            //Top Right
+            Rectangle rectTopRight = new Rectangle(mWidht, rectForm.Y, mWidht, mHeight);
+            DrawPath(rectTopRight, e.Graphics, fbColors.TopRightColor);
+            //Bottom Left
+            Rectangle rectBottomLeft = new Rectangle(rectForm.X, rectForm.X + mHeight, mWidht, mHeight);
+            DrawPath(rectBottomLeft, e.Graphics, fbColors.BottomLeftColor);
+            //Bottom Right
+            Rectangle rectBottomRight = new Rectangle(mWidht, rectForm.Y + mHeight, mWidht, mHeight);
+            DrawPath(rectBottomRight, e.Graphics, fbColors.BottomRightColor);
+            //-> SET ROUNDED REGION AND BORDER
+            FormRegionAndBorder(this, borderRadius, e.Graphics, borderColor, borderSize);
+        }
+        private void FormDanhSachChiTietTienNghi_Resize(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        private void FormDanhSachChiTietTienNghi_SizeChanged(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        private void FormDanhSachChiTietTienNghi_Activated(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+        private void PanelBackground_Paint(object sender, PaintEventArgs e)
+        {
+            ControlRegionAndBorder(PanelBackground, borderRadius - (borderSize / 2), e.Graphics, borderColor);
+        }
+        private void PanelBackground_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void FormDanhSachChiTietTienNghi_Load(object sender, EventArgs e)
+        {
+            grid.ColumnHeadersDefaultCellStyle.Font = new Font(grid.Font, FontStyle.Bold);
+            //Test 
+            grid.Rows.Add(new object[] { "Ti vi", "1", "Sử dụng tốt", edit, delete });
+            grid.Rows.Add(new object[] { "Tủ lạnh", "1", "Đang sửa", edit, delete });
+            grid.Rows.Add(new object[] { "Điều hòa", "2", "Sử dụng tốt", edit, delete });
+            grid.Rows.Add(new object[] { "Máy sấy", "3", "Đang sửa", edit, delete });
+        }
+
+        private void grid_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int x = e.ColumnIndex, y = e.RowIndex;
+            if (y >= 0)
+            {
+                // If click Update button 
+                if (x == 3)
+                {
+                    FormBackground formBackground = new FormBackground(formMain);
+                    try
+                    {
+                        using (FormSuaChiTietTienNghi formSuaChiTietTienNghi = new FormSuaChiTietTienNghi())
+                        {
+                            formBackground.Owner = formMain;
+                            formBackground.Show();
+                            formSuaChiTietTienNghi.Owner = formBackground;
+                            formSuaChiTietTienNghi.ShowDialog();
+                            formBackground.Dispose();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "THÔNG BÁO");
+                    }
+                    finally { formBackground.Dispose(); }
+                }
+                if (x == 4)
+                {
+                    // If click Delete button 
+                    MessageBox.Show("Clicked Delete button");
+                }
+            }
         }
 
         private void CTButtonThemChiTietTienNghi_Click(object sender, EventArgs e)
@@ -49,102 +275,10 @@ namespace HotelManagement.GUI
             finally { formBackground.Dispose(); }
         }
 
-        private void FormDanhSachChiTietTienNghi_Load(object sender, EventArgs e)
-        {
-            grid.ColumnHeadersDefaultCellStyle.Font = new Font(grid.Font, FontStyle.Bold);
-            //Test 
-            grid.Rows.Add(new object[] { TN, "TN001", "Ti vi", "1", "Sử dụng tốt", edit, delete });
-            grid.Rows.Add(new object[] { TN, "TN002", "Tủ lạnh", "1", "Đang sửa", edit, delete });
-            grid.Rows.Add(new object[] { TN, "TN003", "Điều hòa", "2", "Sử dụng tốt", edit, delete });
-            grid.Rows.Add(new object[] { TN, "TN004", "Máy sấy", "3", "Đang sửa", edit, delete });
-        }
-
-        private void buttonExport_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                if (grid.Rows.Count > 0)
-                {
-                    Microsoft.Office.Interop.Excel.Application XcelApp = new Microsoft.Office.Interop.Excel.Application();
-                    XcelApp.Application.Workbooks.Add(Type.Missing);
-
-                    int row = grid.Rows.Count;
-                    int col = grid.Columns.Count; 
-
-                    // Get Header text of Column
-                    for (int i = 1; i < col - 2 + 1; i++)
-                    {
-                        if (i == 1) continue;
-                        XcelApp.Cells[1, i - 1] = grid.Columns[i - 1].HeaderText;
-                    }
-
-                    // Get data of cells
-                    for (int i = 0; i < row; i++)
-                    {
-                        for (int j = 1; j < col - 2; j++)
-                        {
-                            XcelApp.Cells[i + 2, j] = grid.Rows[i].Cells[j].Value.ToString();
-                        }
-                    }
-
-                    XcelApp.Columns.AutoFit();
-                    XcelApp.Visible = true;
-                }
-                else
-                {
-                    string mess = "Chưa có dữ liệu trong bảng!";
-                    int x = 105, y = 60;
-                    using (FormMessageBoxThongBao frm = new FormMessageBoxThongBao(mess, x, y))
-                    {
-                        frm.ShowDialog();
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        private void grid_CellClick(object sender, DataGridViewCellEventArgs e)
-        {
-            int x = e.ColumnIndex, y = e.RowIndex;
-            if (y >= 0)
-            { 
-                // If click Update button 
-                if (x == 5)
-                {
-                    FormBackground formBackground = new FormBackground(formMain);
-                    try
-                    {
-                        using (FormSuaChiTietTienNghi formSuaChiTietTienNghi = new FormSuaChiTietTienNghi())
-                        {
-                            formBackground.Owner = formMain;
-                            formBackground.Show();
-                            formSuaChiTietTienNghi.Owner = formBackground;
-                            formSuaChiTietTienNghi.ShowDialog();
-                            formBackground.Dispose();
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show(ex.Message, "THÔNG BÁO");
-                    }
-                    finally { formBackground.Dispose(); }
-                }
-                if (x == 6)
-                {
-                    // If click Delete button 
-                    MessageBox.Show("Clicked Delete button");
-                }
-            }
-
-        }
-
         private void grid_CellMouseMove(object sender, DataGridViewCellMouseEventArgs e)
         {
             int y = e.RowIndex, x = e.ColumnIndex;
-            if (y >= 0 && x == 5 || y >= 0 && x == 6 || y == -1 && x == 1)
+            if (y >= 0 && x == 3 || y >= 0 && x == 4)
                 grid.Cursor = Cursors.Hand;
             else
                 grid.Cursor = Cursors.Default;
@@ -153,6 +287,11 @@ namespace HotelManagement.GUI
         private void grid_CellMouseLeave(object sender, DataGridViewCellEventArgs e)
         {
             grid.Cursor = Cursors.Default;
+        }
+
+        private void CTButtonLuu_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
